1aa368491b575dece63450c83f8671c570dd205f
Alex Markov <pd.fareny@gmail.com>
2022-03-19 20:48:28 +0300

    Сделал 2 слайдера

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,7 +1,7 @@
 const slider = () => {
     const container = document.querySelector("#benefits > div");
     container.style.width = '585px';
-    const swiper1 = new Swiper('#services .swiper', {
+    const swiperServices = new Swiper('#services .swiper', {
         direction: "horizontal",
         loop: true,
         slidesPerView: 2,
@@ -10,7 +10,7 @@ const slider = () => {
             prevEl: '.services__arrow.services__arrow--left',
         },
     });
-    const swiper2 = new Swiper('#benefits > div > div.benefits-inner.swiper', {
+    const swiperBenefits = new Swiper('#benefits > div > div.benefits-inner.swiper', {
         direction: "horizontal",
         slidesPerGroup: 3,
         loop: true,
